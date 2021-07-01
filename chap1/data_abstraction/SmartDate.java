f5ff33d131a5d2bbbbff69b673b4e0046da0f219
breakthatbass <gamache.taylor@gmail.com>
2021-07-01 13:57:39 -0400

    add ex 12

--- chap1/data_abstraction/SmartDate.java
+++ chap1/data_abstraction/SmartDate.java
@@ -5,8 +5,21 @@
  * if the date is not legal
  *
  * Date class is on page 91
+ *
+ * exercise 1.2.12
+ *
+ * add a method `dayOfTheWeek()` to SmartDate that returns a string value
+ * monday, tuesday, wednesday, thursday, friday, saturday, or sunday, giving the
+ * appropriate day of the week for the date. you may assume that the date is in
+ * the 21st century.
+ *
+ * there is a Date class in the algs4 lib, however, it doesn't really do what we
+ * would need it for here. i found a solution by just using the java stdlib.
  */
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.*;
 
 public class SmartDate
 {
@@ -43,6 +56,29 @@ public class SmartDate
 		return month() + "/" + day() + "/" + year(); 
 	}
 
+
+	// dayOfTheWeek: print the day of the week of the date in instance
+	public String dayOfTheWeek() {
+		
+		// Calendar class returns days as ints 1..7 so there needs to be
+		// something here at the 0 index
+		String[] weekdays = {
+			null, "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+		};
+
+		try {
+			Calendar tmp = new GregorianCalendar(this.year, this.month, this.day);
+			int dayOfWeek = tmp.get(Calendar.DAY_OF_WEEK);
+			return weekdays[dayOfWeek];
+
+		} catch (Exception e) {
+			return "error: dayOfTheWeek: failed to return day";
+		}
+	}
+
+
+	// isValid: check if the date is a valid date
+	// ...does not check for leap years, aint nobody got time for that
 	private boolean isValid(int month, int day) {
 		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
@@ -57,10 +93,14 @@ public class SmartDate
 	}
 
 
+	
+	/******* MAIN **********/
+
 	public static void main(String[] args)
 	{
 		try { // this will work
-			SmartDate works = new SmartDate(7, 1, 2021);
+			SmartDate works = new SmartDate(6, 28, 2021);
+			System.out.println(works.toString() + " -> " + works.dayOfTheWeek());
 		}
 		catch (IllegalArgumentException ex) {
 			System.out.println(ex.getMessage());
