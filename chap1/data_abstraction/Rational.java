7d19b2373edf00c6c0ffbbdb60973ca9ea9f3729
breakthatbass <gamache.taylor@gmail.com>
2021-07-07 16:26:40 -0400

    exercise 17

--- chap1/data_abstraction/Rational.java
+++ chap1/data_abstraction/Rational.java
@@ -4,7 +4,11 @@
  * rational numbers
  * implement an immutable data type Rational for rational numbers that supports
  * addition, subtraction, multiplication, and division.
- *  
+ * 
+ * exercise 1.2.17
+ *
+ * robust rational: use assertions to develop an implmentation of rational that
+ * is immune to overflow 
  */
 
 public class Rational
@@ -45,12 +49,25 @@ public class Rational
 		return denominator;
 	}
 
+	private boolean test_for_overflow(int a)
+	{
+		if (a > Integer.MAX_VALUE) return true;
+		if (a < Integer.MIN_VALUE) return true;
+
+		return false;
+	}
+
 	/***** plus, minus, times, dividedBy, equals, toString  *****/
 
 	public Rational plus(Rational that)
 	{
+		
 		int numer1 = this.numerator() * that.denominator();
 		int numer2 = this.denominator() * that.numerator();
+		
+		if (test_for_overflow(numer1) || test_for_overflow(numer2)) {
+			throw new IllegalArgumentException("too big or too small");
+		}
 
 		return new Rational(numer1+numer2, this.denominator()*that.denominator());
 	}
@@ -59,6 +76,10 @@ public class Rational
 	{
 		int numer1 = this.numerator() * that.denominator();
 		int numer2 = this.denominator() * that.numerator();
+		
+		if (test_for_overflow(numer1) || test_for_overflow(numer2)) {
+			throw new IllegalArgumentException("too big or too small");
+		}
 
 		return new Rational(numer1-numer2, this.denominator()*that.denominator());
 	}
@@ -67,6 +88,10 @@ public class Rational
 	{
 		int num = this.numerator() * that.numerator();
 		int denom = this.denominator() * that.denominator();
+		
+		if (test_for_overflow(num) || test_for_overflow(denom)) {
+			throw new IllegalArgumentException("too big or too small");
+		}
 
 		return new Rational(num, denom);
 	}
@@ -109,5 +134,17 @@ public class Rational
 
 		System.out.println("numer: " + test.numerator());
 		System.out.println("denom: " + test.denominator());
+
+		System.out.println("testing for overflow...");
+		int too_big = Integer.MAX_VALUE + 1;
+		int too_small = Integer.MIN_VALUE - 1;
+
+		Rational too_big_rational = new Rational(too_big, 1);
+		Rational too_small_rational = new Rational(too_small, 1);
+		
+		Rational junk = new Rational(too_big, 1);
+
+		Rational poo = junk.times(too_big_rational);
+
 	}
 }
