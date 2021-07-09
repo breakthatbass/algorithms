323728659dab4399d4092aec52dc77611b7a5704
breakthatbass <gamache.taylor@gmail.com>
2021-07-09 15:15:19 -0400

    add ex 2

--- chap1/bags_queues_stacks/FixedCapStack.java
+++ chap1/bags_queues_stacks/FixedCapStack.java
@@ -2,7 +2,20 @@
  * exercise 1.3.1
  *
  * add a method isFull() to FixedCapacityOfStrings
+ *
+ *
+ * exercise 1.3.2
+ *
+ * give the output printed bt the java Stack for the input
+ * it was - the best - of times - - - it was - the - - -
+ * 
+ * usage: javac-algs4 FixedCapStack < test.txt
+ *
+ * output:
+ * was best times of the was the it it (0 left on stack)
  */
+
+import edu.princeton.cs.algs4.StdIn;
 
 public class FixedCapStack
 {
@@ -34,10 +47,7 @@ public class FixedCapStack
 		a[n++] = item;
 	}
 
-	public String pop()
-	{
-		return a[--n];
-	}
+	public String pop() { return a[--n]; }
 
 	public static void main(String[] args)
 	{
@@ -48,5 +58,19 @@ public class FixedCapStack
 			s.push("hello");
 		}
 		System.out.println("Full: " + s.isFull());
+
+		
+		/**** exercise 1.3.2  ****/
+
+		FixedCapStack part2 = new FixedCapStack(n);
+
+		while (!StdIn.isEmpty()) {
+			String item = StdIn.readString();
+			if (!item.equals("-"))
+				part2.push(item);
+			else if (!part2.isEmpty())
+				System.out.print(part2.pop() + " ");
+		}
+		System.out.println("(" + part2.size() + " left on stack)");
 	}
 }
