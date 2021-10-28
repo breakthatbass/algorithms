31542b5c25313135d5ea8d4efae2547710d8ac21
breakthatbass <gamache.taylor@gmail.com>
2021-10-28 12:25:20 -0400

    add start of ex 9

--- chap1/bags_queues_stacks/parens.java
+++ chap1/bags_queues_stacks/parens.java
@@ -1,5 +1,7 @@
 /*
- * exercise 1.3.9 write a program that from standard input an expression
+ * exercise 1.3.9 
+ *
+ * write a program that from standard input an expression
  * without left parentheses and prints the equivalent infix expression
  * with the parentheses inserted.
  *
@@ -20,11 +22,47 @@ import edu.princeton.cs.algs4.StdIn;
 
 public class parens 
 {
+	private static int count_parens(String s)
+	{
+		int paren_count = 0;
+		int len = s.length();
+		for (int i = 0; i < len; i++) {
+			char c = s.charAt(i);
+			if (c == ')')
+				paren_count++;
+		}
+		return paren_count;
+	}
+
+	private static void check(String s, int parens)
+	{
+		if (count_parens(s) != parens) {
+			System.out.println(s + " doesn't have " + parens + " parens");
+			System.exit(-1);
+		}
+	}
+
+
 	public static void main(String[] args)
 	{
+		String test1 = "1 + 1)";
+		String test2 = "1 + 1) * 2 + 3)))";
+		String test3 = "1 + 2) * 3 - 4) * 5 - 6)))";
+
+
+		check(test1, 1);
+		check(test2, 4);
+		check(test3, 5);
+
+
+
+
+		/*
+		String in = null;
 		while (!StdIn.isEmpty()) {
-			String in = StdIn.readString();
+			in = StdIn.readString();
 			System.out.println(in);
 		}
+		*/
 	}
 }
